26ac292577ab93220c6e3bc04df3ff17dc1b8cb5
Mohamed Abd Elbaset Fawy <Mohamed.Fawy@hadafsolutions.com>
2023-11-03 19:15:02 +0200

    commit

--- Multi-Step-Registration/Forms/Step1.aspx.cs
+++ Multi-Step-Registration/Forms/Step1.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.UI;
 
 namespace Multi_Step_Registration.Forms
@@ -13,8 +14,14 @@ namespace Multi_Step_Registration.Forms
             {
                 Session["FullName"] = fullNameTextBox.Text;
                 Session["Address"] = addressDropDownList.SelectedValue;
+                string currentUrl = HttpContext.Current.Request.Url.AbsoluteUri;
+                string nextPageUrl;
+                if (currentUrl.Contains("Forms"))
+                    nextPageUrl = currentUrl.Replace("Step1.aspx", "Step2.aspx");
+                else
+                    nextPageUrl = "./Forms/Step2.aspx";
 
-                Response.Redirect("./Step2.aspx");
+                Response.Redirect(nextPageUrl);
             }
         }
     }
